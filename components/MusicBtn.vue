ed76c7e77d0ac9d9b682c3d573e6d37969c2721b
YernarT <toktaryernar@gmail.com>
2023-07-10 01:29:15 +0600

    feat: :sparkles: music button

---
+++ components/MusicBtn.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="music-btn" @click="handleClick">
+    <img
+      v-show="!isPlayed"
+      class="btn-image"
+      src="~/assets/image/play.png"
+      alt="Play Btn"
+    />
+    <img
+      v-show="isPlayed"
+      class="btn-image"
+      src="~/assets/image/pause.png"
+      alt="Pause Btn"
+    />
+    <img
+      class="btn-help-text"
+      src="~/assets/image/help-text.png"
+      alt="Help text"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const isPlayed = ref(false);
+
+const handleClick = () => {
+  isPlayed.value = !isPlayed.value;
+};
+</script>
+
+<style scoped lang="scss">
+@import "~/assets/style/mixins.scss";
+
+.music-btn {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  cursor: pointer;
+
+  .btn-image {
+    width: 65%;
+    @include positionCenter();
+  }
+
+  .btn-help-text {
+    width: 100%;
+    animation: rotate 10s linear infinite;
+    @include positioned();
+  }
+
+  @keyframes rotate {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+}
+</style>
