c751bd5a1a6e872238665cf62f5d2eed883277e2
YernarT <toktaryernar@gmail.com>
2023-07-09 11:07:37 +0600

    init: :rainbow: init nuxt3 project

---
+++ app.vue
@@ -0,0 +1,50 @@
+<template>
+  <Html lang="kk">
+    <Head>
+      <Meta charset="UTF-8" />
+      <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <Meta name="theme-color" content="#f7f7f7" />
+      <Meta name="color-scheme" content="light" />
+
+      <Meta httpEquiv="x-ua-compatible" content="IE=edge" />
+      <Meta httpEquiv="x-ua-compatible" content="IE=7" />
+
+      <Meta name="google" content="notranslate" />
+      <Meta name="description" content="Самғар мен Айшат үйлену тойы" />
+      <Meta name="keywords" content="Той Бағақ" />
+      <Meta name="author" content="https://github.com/YernarT" />
+
+      <Meta property="og:title" content="Самғар & Айшат" />
+      <Meta property="og:description" content="Самғар мен Айшат үйлену тойы" />
+      <Meta property="og:image" content="/favicon.ico" />
+
+      <Link rel="icon" href="/favicon.ico" />
+
+      <Link rel="preconnect" href="https://fonts.googleapis.com" />
+      <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+      <Link
+        href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates&display=swap"
+        rel="stylesheet"
+      />
+
+      <Title>Самғар & Айшат</Title>
+    </Head>
+
+    <Body>
+      <NuxtPage />
+    </Body>
+  </Html>
+</template>
+
+<script setup>
+import { onMounted, onBeforeUnmount } from '.nuxt/imports';
+
+
+</script>
+
+<style lang="scss">
+* {
+  /* background: red; */
+  /* 鼠标点击任何区域，冒出爱心 */
+}
+</style>
