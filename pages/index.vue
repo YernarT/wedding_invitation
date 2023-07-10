9bb3d938e4567a4b2b3f10e740e1e9a19eefdceb
YernarT <toktaryernar@gmail.com>
2023-07-10 13:21:22 +0600

    init: :rainbow: index page

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="index-page">
+    <ImageSection />
+    <InfoSection />
+    <CountDownSection />
+    <AreYouComming />
+    <ToyGallery />
+    <Footer />
+  </main>
+</template>
+
+<script setup lang="ts">
+import { onMounted, onBeforeUnmount } from "vue";
+</script>
+
+<style scoped lang="scss">
+@import "~/assets/style/mixins.scss";
+
+.index-page {
+  overflow: hidden;
+}
+
+/* @TODO: 鼠标点击任何区域，冒出爱心 */
+</style>
