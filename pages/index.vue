da152303a2f618e92683e37e8bab99a7164cb897
YernarT <toktaryernar@gmail.com>
2023-07-11 02:28:02 +0600

    feat: :sparkles: global click effect

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,31 @@
 
 <script setup lang="ts">
 import { onMounted, onBeforeUnmount } from "vue";
+import heartSVG from "~/assets/image/heart.svg?inline";
+
+const globalClickHanlder = (e: MouseEvent) => {
+  const { clientX, clientY } = e;
+
+  const heartDOM = new Image(18, 18);
+  heartDOM.src = heartSVG;
+  heartDOM.classList.add("itisit-heart");
+  heartDOM.style.cssText = `
+    --x: ${clientX}px;
+    --y: ${clientY}px;
+  `;
+
+  document.body.appendChild(heartDOM);
+  setTimeout(() => {
+    heartDOM.remove();
+  }, 666 + 88);
+};
+
+onMounted(() => {
+  document.addEventListener("click", globalClickHanlder);
+});
+onBeforeUnmount(() =>
+  document.removeEventListener("click", globalClickHanlder)
+);
 </script>
 
 <style scoped lang="scss">
